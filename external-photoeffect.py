e74f8e25ea835857f64cfa36bc66f25f67cd1cec
Vadim Fedulov <vadimfedulov.bmstu@gmail.com>
2026-02-12 16:43:34 +0300

    Add comments in external-photoeffect.py

--- external-photoeffect.py
+++ external-photoeffect.py
@@ -1,3 +1,4 @@
+# Подключение необходимых пакетов
 import locale
 from pathlib import Path
 
@@ -9,33 +10,44 @@ from scipy import stats
 from scipy.constants import c, e, h
 from uncertainties import ufloat
 
+# Настройка локализации
 locale.setlocale(locale.LC_ALL, "ru_RU.UTF-8")
-ureg = pint.UnitRegistry(fmt_locale="ru_RU.UTF-8")
-ureg.load_definitions("units.txt")
-ureg.formatter.default_format = "~"
-pint_pandas.PintType.ureg = ureg
+# Настройка единиц измерения
+ureg = pint.UnitRegistry(fmt_locale="ru_RU.UTF-8")  # русский язык единиц измерения
+ureg.load_definitions("units.txt")  # загрузка пользовательских единиц измерения
+ureg.formatter.default_format = "~"  # отображение сокращённых единиц измерения
+pint_pandas.PintType.ureg = ureg  # настройка единиц измерения для pandas
 
+# Директории
 measurements_dir = Path("measurements")
 output_dir = Path(".")
 
+# Чтение данных (измерений)
 data = pd.read_csv(
     measurements_dir / "435nm.csv", header=[0, 1], dtype=float
 ).pint.quantify(level=1)
 
-voltmeter_error = 0.1 * ureg.V
+# Приборная погрешность:
+# в случае цифрового вольтметра - единица наименьшего разряда
+voltmeter_error = 0.1 * ureg.V  # в данном случае - 0,1 В
+
+# Массив напряжений из таблицы
 voltages = data["Voltage"]
 
+### РАСЧЁТЫ ###
 # Среднее напряжение
 mean_voltage = voltages.mean()
-# Коэффициент Стьюдента
+# Коэффициент Стьюдента t_student
 confidence = 0.95
 n = len(voltages)
 t_student = stats.t.ppf((1 + confidence) / 2, n - 1)
 # Случайная (статистическая) погрешность
+# - std - standard deviation - стандартное (среднеквадратическое) отклонение
 statistical_error = t_student * voltages.std() / np.sqrt(n)
 # Полная погрешность
 total_error = np.sqrt(statistical_error**2 + voltmeter_error**2)
 
+# Задерживающее напряжение с учётом абсолютной ошибки
 U_mag = ufloat(mean_voltage.magnitude, total_error.magnitude)
 U = U_mag * ureg.V
 
@@ -59,7 +71,7 @@ freq = c / wavelength
 # Работа выхода
 A = h * freq - e * U
 
-# Добавление результатов в файл
+# Добавление итоговых результатов в файл
 with open(fname, "a", encoding="utf-8") as f:
     f.write(f"\nЧастота фотона: ν = {freq.to('Hz'):~e}\n")
     f.write(f"Энергия фотона: E = {(h * freq).to('eV')}\n")
